625aa7aab4b069bc93b460bb9929baf87ee839e3
zadubanm02 <zadubanm02@gmail.com>
2020-11-14 18:31:47 +0100

    Db seeder

---
+++ database/factories/EmployeeFactory.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Employee;
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Str;
+
+class EmployeeFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Employee::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'firstName' => $this->faker->firstName,
+            'lastName' => $this->faker->lastName,
+            'email' => $this->faker->unique()->safeEmail,
+            'position' => $this->faker->jobTitle,
+            'department' => 'HR',
+            'salary' => $this->faker->numberBetween($min = 1000, $max = 2500),
+        ];
+    }
+}
